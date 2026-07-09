23d4fbb00d287f0bcb6a2840f95708c3ebd7a280
yuktha809 <yuktha809@gmail.com>
2026-07-09 14:31:05 +0530

    Time: 79 ms (66.29%), Space: 88.9 MB (49.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& a) {
-        int n = a.size();
-        // If the array is empty
-        if (n == 0) return 0; 
-    
-        // Initialize the longest sequence length
-        int longest = 1; 
+    int longestConsecutive(vector<int>& nums) {
+       int n = nums.size();
+       if(n == 0) return 0;
+       int longest = 1;
         unordered_set<int> st;
-    
-        // Put all the array elements into the set
-        for (int i = 0; i < n; i++) {
-            st.insert(a[i]);
+        for(int i = 0;i<n;i++){
+            st.insert(nums[i]);
         }
-    
-        /* Traverse the set to 
-           find the longest sequence  */
-        for (auto it : st) {
-            // Check if 'it' is a starting number of a sequence
-            if (st.find(it - 1) == st.end()) {
-                // Initialize the count of the current sequence
-                int cnt = 1; 
-                // Starting element of the sequence
-                int x = it; 
-    
-                // Find consecutive numbers in the set
-                while (st.find(x + 1) != st.end()) {
-                    // Move to the next element in the sequence
-                    x = x + 1; 
-                    // Increment the count of the sequence
-                    cnt = cnt + 1; 
+        for( auto num : st){
+            if(st.find(num - 1 )== st.end()){
+                int count =1;
+                int x = num;
+                while(st.find(x+1)!=st.end()){
+                    x += 1;
+                    count +=1;
                 }
-                // Update the longest sequence length
-                longest = max(longest, cnt);
+                longest = max(longest,count);
             }
+            
         }
         return longest;
     }
