8c0d4d7bf36f6251d15ff3bcea92b993f5949880
yuktha809 <yuktha809@gmail.com>
2026-07-31 11:53:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.31%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         string ans = "";
-        int count = 0;
+        int count =0;
         for(char ch : s){
-            if(ch == '('){
-                 if(count>0){
-                    ans += ch;
-            }
-            count++;
+            if(ch=='('){
+                if(count>0){
+                    ans +=ch;
+                }
+                count++;
             }
             else{
                 count--;
-                if(count > 0){
-                    ans += ch;
+                if(count>0){
+                    ans +=ch;
                 }
-
+                
             }
         }
-    return ans;
-
-        }
+        return ans;
+    }
 };
