4897eb2fc12cd6ba87945e0b981747293f13e8a1
yuktha809 <yuktha809@gmail.com>
2026-08-05 20:38:24 +0530

    Time: 2 ms (50.2%), Space: 9.7 MB (50.07%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        int freq[26] ={0};
+        for(int i =0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+            
+        }
+        for(int i =0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
